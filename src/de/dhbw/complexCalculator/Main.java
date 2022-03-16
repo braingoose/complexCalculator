c30c906d395970bcc58e34de05a8ec3b230dada7
DHBW-TEL19GR2-et19114 <65611210+DHBW-TEL19GR2-et19114@users.noreply.github.com>
2022-03-16 13:37:57 +0100

    Fix multiplication bug, modify display method

--- src/de/dhbw/complexCalculator/Main.java
+++ src/de/dhbw/complexCalculator/Main.java
@@ -5,10 +5,13 @@ import javax.swing.*;
 public class Main {
 
     public static void main(String[] args) {
-
         try {
             ComplexCalculatorGUI complexCalculatorGUI = new ComplexCalculatorGUI();
             complexCalculatorGUI.initializeComplexCalculator();
+
+            ComplexNumber z1 = new ComplexNumber(0, 0.3333333333333);
+            ComplexNumber res = z1.mul(new ComplexNumber(3,0));
+            System.out.println(res);
         } catch (Exception e) {
             JOptionPane.showMessageDialog(new JFrame(), e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
         }
